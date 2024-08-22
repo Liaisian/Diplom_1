05a7c07042d5f56f53ae39fbb38e93626c2f638b
Lyaisyan Zalotdinova <zalotdinova.l@yandex.ru>
2024-08-22 18:13:07 +0300

    new commit

--- tests/bun_test.py
+++ tests/bun_test.py
@@ -1,26 +1,29 @@
+import pytest
+from praktikum.bun import Bun
 class TestBun:
 
     @pytest.mark.parametrize('name, price', [
-        ('Classic Bun', 2.5),
-        ('PraktiCUM Bun', 3),
+        ('Bun 1', 50.0),
+        ('Bun 2', 65.5),
+        ('Bun 3', None),
+        ('Bun 4', -1),
+        (1, '100'),
         (None, 0.0),
-        ('Null price Bun', None),
-        (None,None),
-        (123,'1.2'),
+        (None, None),
     ])
-    def test_get_name(self, name, price):
+    def test_bun_name(self, name, price):
         bun = Bun(name, price)
         assert bun.get_name() == name
 
-
     @pytest.mark.parametrize('name, price', [
-        ('Classic Bun', 2.5),
-        ('PraktiCUM Bun', 3),
+        ('Bun 1', 50.0),
+        ('Bun 2', 65.5),
+        ('Bun 3', None),
+        ('Bun 4', -1),
+        (1, '100'),
         (None, 0.0),
-        ('Null price Bun', None),
-        (None,None),
-        (123,'1.2'),
+        (None, None),
     ])
-    def test_get_price(self, name, price):
+    def test_bun_price(self, name, price):
         bun = Bun(name, price)
         assert bun.get_price() == price
